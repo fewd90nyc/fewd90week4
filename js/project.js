e6fc7cdf8d37a70714e0facae3ff735bf0000ef8
sabgab <sabrina_gaber@yahoo.com>
2016-07-19 18:24:08 -0400

    Week 4 HW

---
+++ js/project.js
@@ -0,0 +1,19 @@
+var inputFtoC;
+var inputCtoF;
+
+
+function calculateTotal () {
+	inputFtoC = document.getElementById("inputFtoC").value;
+	inputCtoF = document.getElementById("inputCtoF").value;
+
+	var totalFtoC = (inputFtoC - 32) * (5/9);
+	var totalCtoF = (inputCtoF / (5/9) + 32);
+
+	document.getElementById("solutionFtoC").innerHTML = totalFtoC;
+	document.getElementById("solutionCtoF").innerHTML = totalCtoF;
+
+}
+
+document.getElementById("convertFtoC").onclick = calculateTotal;
+document.getElementById("convertCtoF").onclick = calculateTotal;
+
