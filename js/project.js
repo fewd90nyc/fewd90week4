b253e317b5a162afe1cc292abad635fdbc100bb6
robertohernandez11 <roberto@Robertos-Air.us.sinet.si.edu>
2016-07-19 13:30:06 -0400

    js update

--- js/project.js
+++ js/project.js
@@ -1,17 +1,16 @@
 
  function convertFtoC() {
-  var fTemp = document.getElementById("inputFtoC").value;
-  var fToCel = (fTemp - 32) * 5 / 9;
-  var convertFtoC = fTemp+'\xB0F is ' + fToCel + '\xB0C.';
-    document.getElementById("convertFtoC").innerHTML = document.getElementById("solutionFtoC").value;
-    
+  	var userinput = document.getElementById("inputFtoC").value;
+  	var result = (userinput - 32) * 5 / 9;
+ 	document.getElementById("solutionFtoC").innerHTML = result;
 } 	
 
+document.getElementById('solutionFtoC').onclick = convertFtoC;
 
  function convertCtoF() {
-    var inputCtoF = document.getElementById("inputCtoF").celsius;
-    var cToFahr = cTemp * 9 / 5 + 32;
-    var message = cTemp+'\xB0C is ' + cToFahr + ' \xB0F.';
-   }
+    var userinput = document.getElementById("inputCtoF").value;
+    var result = userinput * 9 / 5 + 32;
+    document.getElementById("solutionCtoF").innerHTML = result;
+}
 
-
+document.getElementById('solutionCtoF').onclick = convertCtoF;
